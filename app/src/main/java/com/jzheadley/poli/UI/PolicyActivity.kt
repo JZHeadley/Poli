b03f083e781349b43f1d55ef949cebb0557fe4bf
Jonathan <samsonjj@vcu.edu>
2018-03-04 04:02:24 -0500

    color change

--- app/src/main/java/com/jzheadley/poli/UI/PolicyActivity.kt
+++ app/src/main/java/com/jzheadley/poli/UI/PolicyActivity.kt
@@ -7,6 +7,9 @@ import android.util.Log
 import android.widget.*
 import com.jzheadley.poli.R
 import kotlinx.android.synthetic.main.policy_activity.*
+import android.text.method.ScrollingMovementMethod
+
+
 
 
 class PolicyActivity : AppCompatActivity() {
@@ -14,6 +17,7 @@ class PolicyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.policy_activity)
+        policyTextView.setMovementMethod(ScrollingMovementMethod())
     }
 
 }
