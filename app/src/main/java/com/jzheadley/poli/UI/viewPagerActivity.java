7a55b0b3d705e4c170765b9ab5e01f2392992e25
Bhargav Sathish <sathishb@mymail.vcu.edu>
2018-03-04 09:17:57 -0500

    added everything for viewPager

---
+++ app/src/main/java/com/jzheadley/poli/UI/viewPagerActivity.java
@@ -0,0 +1,22 @@
+package com.jzheadley.poli.UI;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.jzheadley.poli.R;
+
+public class viewPagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pager);
+        ViewPager vpPager = (ViewPager) findViewById(R.id.vpPager);
+    }
+}
